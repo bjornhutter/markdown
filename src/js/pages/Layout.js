d6b73ae7cae31d849b7d95bd941ad36d1898a905
Björn Hutter <bjornhutter@gmail.com>
2016-11-10 15:12:39 +0100

    Changed the default content of the editor

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -17,7 +17,36 @@ export default class Layout extends React.Component {
     constructor(props) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
-        this.state = {value: 'Type some *markdown* **here!**'};
+        this.state = {value:
+        '# Heading ' +
+        '\n## Sub-heading ' +
+        '\n### We gotta go deeper ' +
+        '\n--- ' +
+        '\n#### Deeper ' +
+        '\n##### Sooooo deeeeep ' +
+        '\n###### Not much deeper than this though. ' +
+        '\n___ ' +
+        '\n' +
+        '\nThis is some *italic*, **bold** and **__strong emphasis__** ~~delete this~~ ' +
+        '\n' +
+        '\n1. Ordered list-item ' +
+        '\n2. Ordered list-item ' +
+        '\n3. Ordered list-item ' +
+        '\n' +
+        '\n* Unordered list-item ' +
+        '\n* Unordered list-item ' +
+        '\n* Unordered list-item ' +
+        '\n*** ' +
+        '\n' +
+        '\nThis is a `inline code` example ' +
+        '\n' +
+        '\n``` javascript ' +
+        '\n/* This is a code block */ ' +
+        '\nvar foo = "bar"; ' +
+        '\nalert(foo); ``` ' +
+        '\n' +
+        '\n#### [Checkout this project on GitHub](https://github.com/bjornhutter/markdown) ' +
+        '![alt text](http://bjornhutter.com/img/logos/bjorn_hutter_fav_icon_black.png "Bjorn Hutter Favicon") '};
     }
 
     handleChange() {
@@ -34,7 +63,9 @@ export default class Layout extends React.Component {
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="text-center">React GitHub Markdown App</h1>
-                        <h4 class="text-center"><a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">Markdown Cheatsheet</a></h4>
+                        <h4 class="text-center"><a
+                            href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet" target="_blank">Markdown
+                            Cheatsheet</a></h4>
                         <div class="col-lg-6">
                             <h3>Input</h3>
                             <textarea class="form-control" rows="20"
